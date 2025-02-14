628f611601b458b6c8a34dfeeb64503f5f935041
Yang Bo <yb18810279667@163.com>
2025-02-14 11:09:30 +0800

    添加向量库

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ from langchain_core.output_parsers import StrOutputParser
 from langchain.prompts import PromptTemplate
 from langchain.chains import RetrievalQA
 import sys
-sys.path.append("../C3 搭建知识库") # 将父目录放入系统路径中
+# sys.path.append("../C3 搭建知识库") # 将父目录放入系统路径中
 from zhipuai_llm import ZhipuAILLM
 from zhipuai_embedding import ZhipuAIEmbeddings
 from langchain.vectorstores.chroma import Chroma
